332d64bc5d5e03030c6532c303a0de28a3a5e018
Sanm-ZH <zhxpvip7@gmail.com>
2021-03-07 20:17:19 +0800

    fix 打印转义问题

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ pnpm build
 
 git push origin master:master
 pnpx gh-pages -d docs/dist
-echo -e "\e[32m 已部署成功至sanm-zh.github.io"
+echo -e '\033[32m 已部署成功至sanm-zh.github.io'
 
 git remote set-url origin git@gitee.com:sanm-zh/sanm-zh.git
 git push origin master:master
 pnpx gh-pages -d docs/dist
-echo -e "\e[32m 已部署成功至sanm-zh.gitee.io"
-echo -e "\e[32m 如果不是gitee pro用户请手动更新gitee pages服务"
+echo -e '\033[32m 已部署成功至sanm-zh.gitee.io'
+echo -e '\033[32m 如果不是gitee pro用户请手动更新gitee pages服务'
 
 git remote set-url origin git@github.com:Sanm-ZH/Sanm-ZH.github.io.git
