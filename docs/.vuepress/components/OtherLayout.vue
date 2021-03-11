9726f68d595374d00ebab001a18442889c7e4eff
Sanm-ZH <zhxpvip7@gmail.com>
2021-03-11 16:20:20 +0800

    update 样式调整&新增一个其他模板

---
+++ docs/.vuepress/components/OtherLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <Common
+    :sidebar="false"
+    :isComment="false"
+  >
+  </Common>
+</template>
+
+<script>
+import Common from "@theme/components/Common.vue";
+export default {
+  name: "OtherLayout",
+  components: {
+    Common,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="css" scope>
+a {
+  font-weight: 500;
+  color: #37aabb;
+  text-decoration: none;
+}
+a > i,
+.title > i {
+  font-size: 13px !important;
+}
+.color-button > i {
+  font-size: 22.4px;
+}
+.navbar {
+  position: fixed;
+  z-index: 20;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 3.6rem;
+  box-sizing: border-box;
+}
+.navbar {
+  padding: 0.7rem 1.5rem;
+  line-height: 2.2rem;
+}
+.nav-links a {
+  line-height: 1.4rem;
+}
+</style>
--- docs/.vuepress/components/Projects.vue
+++ docs/.vuepress/components/Projects.vue
@@ -69,6 +69,34 @@ export default {
 </script>
 
 <style lang="css" scope>
+a {
+  font-weight: 500;
+  color: #37aabb;
+  text-decoration: none;
+}
+a > i,
+.title > i {
+  font-size: 13px !important;
+}
+.color-button > i {
+  font-size: 22.4px;
+}
+.navbar {
+  position: fixed;
+  z-index: 20;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 3.6rem;
+  box-sizing: border-box;
+}
+.navbar {
+  padding: 0.7rem 1.5rem;
+  line-height: 2.2rem;
+}
+.nav-links a {
+  line-height: 1.4rem;
+}
 .project {
   max-width: 1024px;
   min-height: 300px;
