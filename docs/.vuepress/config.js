91796871e18560b935fc2077d64ad2ce8ca0a961
Sanm-ZH <zhxpvip7@gmail.com>
2021-03-26 11:33:33 +0800

    fix 引入复制代码插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -181,11 +181,16 @@ module.exports = {
       keyboard: true, //default false
       previewer: true, //default false
     }],
-    [require('./plugin/markdown-copy/index.js'), {
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: 'Copy successfully and then paste it for use.', // default is 'Copy successfully and then paste it for use.'
-      duration: 300, // prompt message display time.
-      showInMobile: false // whether to display on the mobile side, default: false.
+    // [require('./plugin/markdown-copy/index.js'), {
+    //   copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+    //   copyMessage: 'Copy successfully and then paste it for use.', // default is 'Copy successfully and then paste it for use.'
+    //   duration: 300, // prompt message display time.
+    //   showInMobile: false // whether to display on the mobile side, default: false.
+    // }]
+    ['vuepress-plugin-click-copy-markdown', {
+      title: '复制',
+      tips: '点击复制',
+      showIcon: true
     }]
   ]
 }
