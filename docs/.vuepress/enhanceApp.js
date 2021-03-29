5c87049590f6d9c6a82f92c401fcb42b90fb3625
Sanm-ZH <zhxpvip7@gmail.com>
2021-03-29 17:25:40 +0800

    update 冗余

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,4 @@
 import './public/iconfont/iconfont.css'
-import dayjs from 'dayjs'
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -7,13 +6,5 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  siteData.pages.forEach(item => {
-    // 将时间本地时间转换成UTC时间
-    if (item.frontmatter && item.frontmatter.date) {
-      const utc = require('dayjs/plugin/utc')
-      dayjs.extend(utc)
-      const dateF = dayjs(item.frontmatter.date).utc().format('YYYY/MM/DD HH:mm:ss')
-      item.frontmatter.date = dayjs(dateF).utc().format()
-    }
-  })
+  
 }
